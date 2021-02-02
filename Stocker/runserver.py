4b82ad56be4bbba8d82d71234913fbd05b7834f5
Jacob Cassady <jtcass01@louisville.edu>
2021-02-01 23:27:03 -0500

    Initial commit.  Loaded flask template from visual studio.

---
+++ Stocker/runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the Stocker application using a development server.
+"""
+
+from os import environ
+from Stocker import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
